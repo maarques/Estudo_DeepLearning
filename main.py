cb2a8dbdff3978878df4f73eea0f70b8da431644
maarques <thi4go.marqu3s@gmail.com>
2025-10-20 12:47:36 -0300

    conversa com modelo de ia, via input do usuário

--- main.py
+++ main.py
@@ -4,24 +4,51 @@ import os
 
 load_dotenv()
 
+modelo = "meta-llama/Llama-3.3-70B-Instruct"
+
 client = InferenceClient(
-    model= "meta-llama/Llama-3.3-70B-Instruct",
+    model= modelo,
     api_key=os.getenv("HUGGINGFACE_TOKEN_KEY")
 )
 
-output = client.chat.completions.create(
-    model="meta-llama/Llama-3.3-70B-Instruct",
-    messages=[
-        {
-            "role": "system",
-            "content": "Listar apenas os nomes dos produtos, sem considerar a descrição."
-        },
-        {
-            "role": "user",
-            "content": "Liste 3 produtos sustentáveis."
-        }
-    ],
-    max_tokens=1024
-)
+def category_product(product_name, list_possible_categories):
+    prompt_system = f"""
+        Você é um categorizador de produtos.
+        Você deve assumir as categorias presentes na lista abaixo.
 
-print(output.choices[0].message["content"])
+        # Lista de Categorias Válidas
+            {list_possible_categories.split(",")}
+
+        # Formato da Saída
+            Produto: Nome do Produto
+            Categoria: apresente a categoria do produto
+
+        # Exemplo de Saída
+            Produto: Escova elétrica com recarga solar
+            Categoria: Eletrônicos Verdes
+    """
+
+    output = client.chat.completions.create(
+        model=modelo,
+        messages=[
+            {
+                "role": "system",
+                "content": prompt_system
+            },
+            {
+                "role": "user",
+                "content": product_name
+            }
+        ],
+        max_tokens=1024,
+        temperature=0,
+    )
+
+    return output.choices[0].message["content"]
+
+valid_categories = input("Informe as categorias válidas, separando por vígula: ")
+
+while True:
+    product_name = input("Digite o nome do produto: ")
+    output_text = category_product(product_name, valid_categories)
+    print(output_text)
