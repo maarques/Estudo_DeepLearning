1051b1410323bf7c3ad5a10372e561a8cde64144
maarques <thi4go.marqu3s@gmail.com>
2025-10-23 17:08:50 -0300

    adicionando um contexto ao bot

--- 02-python-e-gpt-crie-seu-chatbot-com-ia/app.py
+++ 02-python-e-gpt-crie-seu-chatbot-com-ia/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, Response
 from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 import os
+from time import sleep
+from helpers import *
 
 load_dotenv()
 
@@ -13,12 +15,58 @@ client = InferenceClient(
 )
 
 app = Flask(__name__)
-app.secret_key = "alura"
+app.secret_key = "meu_nome"
+
+contexto = carrega("02-python-e-gpt-crie-seu-chatbot-com-ia\dados\EcoMart.txt")
+
+def bot(prompt):
+    max_tentativas = 1
+    repeticao = 0
+
+    while True:
+        try:
+            prompt_sistema = f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce. 
+            Você não deve responder perguntas que não sejam dados do e-commerce informado!
+
+            Você deve gerar respostas utilizando o contexto abaixo
+
+            # Contexto
+            {contexto}
+            """
+
+            response = client.chat.completions.create(
+                messages=[
+                        {
+                                "role": "system",
+                                "content": prompt_sistema
+                        },
+                        {
+                                "role": "user",
+                                "content": prompt
+                        }
+                ],
+                temperature=1,
+                max_tokens=300,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                model = modelo)
+            return response
+        except Exception as erro:
+                repeticao += 1
+                if repeticao >= max_tentativas:
+                    return "Erro no GPT: %s" % erro
+                print('Erro de comunicação com OpenAI:', erro)
+                sleep(1)
+             
 
 @app.route("/chat", methods=["POST"])
 def chat():
     prompt = request.json["msg"]
     resposta = bot(prompt)
+    texto_resposta = resposta.choices[0].message["content"]
+    return texto_resposta
 
 @app.route("/")
 def home():
